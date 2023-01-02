1703be660839de4f561ff92db74b94757cddbbdc
Qva777 <barshay.sasha@gmail.com>
2023-01-02 12:00:00 +0200

    Test commit

--- E_Shop_Frontend/Users/serializers.py
+++ E_Shop_Frontend/Users/serializers.py
@@ -1,21 +1,15 @@
-#  ######gg
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
-from django import forms
-from django.core.exceptions import ValidationError
-import re
 from E_Shop_API.E_Shop_Users.models import Clients
+from django.contrib.auth.forms import UserCreationForm
 from E_Shop_API.E_Shop_Users.validators import validate_password
 
 
 class ClientsCreationForm(UserCreationForm):
+    """ Register field """
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput(), validators=[validate_password])
     password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput())
 
     class Meta:
         model = Clients
-        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'birth_date', 'photo', 'disabled']
-#  ######gg
+        fields = ['username', 'first_name', 'last_name', 'email', 'password1', 'password2', 'birth_date', 'photo',
+                  'disabled']
