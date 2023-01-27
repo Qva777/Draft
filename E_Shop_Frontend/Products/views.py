5dd3980afd42d4851f6cb4d8e221f151e3a6d19c
Qva777 <barshay.sasha@gmail.com>
2023-01-27 12:00:00 +0200

    Updated

--- E_Shop_Frontend/Products/views.py
+++ E_Shop_Frontend/Products/views.py
@@ -13,7 +13,6 @@ from django.views.generic import TemplateView
 from E_Shop_API.E_Shop_Products.models import Product
 from django.views import View
 from rest_framework import status
-from rest_framework.views import APIView
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
