4bfdfea7d49a14a58e2965df4cb96999d31c0b47
Qva777 <barshay.sasha@gmail.com>
2023-01-27 12:00:00 +0200

    Updated

--- E_Shop_Frontend/Products/views.py
+++ E_Shop_Frontend/Products/views.py
@@ -1,6 +1,6 @@
 import random
 
-from django.shortcuts import render
+
 from django.views.decorators.csrf import csrf_exempt
 from django.urls import reverse
 from django.shortcuts import get_object_or_404
