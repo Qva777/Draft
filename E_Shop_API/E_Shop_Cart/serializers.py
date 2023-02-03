c492bd0aebb165a213735d5d9f465ef2829d4e28
Qva777 <barshay.sasha@gmail.com>
2023-02-03 12:00:00 +0200

    Updated

--- E_Shop_API/E_Shop_Cart/serializers.py
+++ E_Shop_API/E_Shop_Cart/serializers.py
@@ -1,47 +1,84 @@
-from E_Shop_API.E_Shop_Users.models import Clients
-
-from E_Shop_API.E_Shop_Cart.models import Cart, CartProduct
-from rest_framework import serializers
 
 
-class CartSerializer(serializers.ModelSerializer):
-    """ Post/Get/ Cart"""
-
-    class Meta:
-        model = Cart
-        fields = ('id', 'user', 'created_at', 'total_price')
 
 
-class CartProductSerializer(serializers.ModelSerializer):
-    """ Adds Product into Cart model """
-
-    class Meta:
-        model = CartProduct
-        fields = ('id', 'quantity',)
 
 
-class GetUserSerializer(serializers.ModelSerializer):
-    """ Get username """
-
-    class Meta:
-        model = Clients
-        fields = ('username',)
 
 
-class GetCartUserSerializer(serializers.ModelSerializer):
-    """ Get user """
-    user = GetUserSerializer(read_only=True)
-
-    class Meta:
-        model = Cart
-        fields = ('user',)
 
 
-class CartProductDetailSerializer(serializers.ModelSerializer):
-    """ Show Detail view about CartProduct """
-    cart = GetCartUserSerializer(read_only=True)
-    product = serializers.CharField(source='product.name')
 
-    class Meta:
-        model = CartProduct
-        fields = ('id', 'cart', 'product', 'quantity', 'price', 'total_price')
+
+
+
+
+
+
+
+
+# from E_Shop_API.E_Shop_Users.models import Clients
+#
+# from E_Shop_API.E_Shop_Cart.models import Cart, CartProduct
+# from rest_framework import serializers
+#
+#
+# # class UserSerializer(serializers.ModelSerializer):
+# #     """ User Serializer """
+# #     class Meta:
+# #         model = Clients
+# #         fields = ('id', 'email')
+# #
+# # class CartSerializer(serializers.ModelSerializer):
+# #     """ Post/Get Cart """
+# #     user = UserSerializer(read_only=True)
+# #
+# #     class Meta:
+# #         model = Cart
+# #         fields = ('id', 'user', 'created_at', 'total_price')
+#
+#
+#
+#
+#
+# class CartSerializer(serializers.ModelSerializer):
+#     """ Post/Get/ Cart"""
+#
+#     class Meta:
+#         model = Cart
+#         fields = ('id', 'user', 'created_at', 'total_price')
+#
+#
+# class CartProductSerializer(serializers.ModelSerializer):
+#     """ Adds Product into Cart model """
+#
+#     class Meta:
+#         model = CartProduct
+#         fields = ('id', 'quantity',)
+#
+#
+# class GetUserSerializer(serializers.ModelSerializer):
+#     """ Get username """
+#
+#     class Meta:
+#         model = Clients
+#         fields = ('username',)
+#
+#
+# class GetCartUserSerializer(serializers.ModelSerializer):
+#     """ Get user """
+#     user = GetUserSerializer(read_only=True)
+#
+#     class Meta:
+#         model = Cart
+#         fields = ('user',)
+#
+#
+# class CartProductDetailSerializer(serializers.ModelSerializer):
+#     """ Show Detail view about CartProduct """
+#     cart = GetCartUserSerializer(read_only=True)
+#     product = serializers.CharField(source='product.name')
+#
+#     class Meta:
+#         model = CartProduct
+#         fields = ('id', 'cart', 'product', 'quantity', 'price', 'total_price')
