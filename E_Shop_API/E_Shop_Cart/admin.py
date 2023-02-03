aeeab8e10d8aafc9d8669f3637d38f2a93ded782
Qva777 <barshay.sasha@gmail.com>
2023-02-03 12:00:00 +0200

    Updated

--- E_Shop_API/E_Shop_Cart/admin.py
+++ E_Shop_API/E_Shop_Cart/admin.py
@@ -1,18 +1,24 @@
-from django.contrib import admin
 from E_Shop_API.E_Shop_Cart.models import Cart, CartProduct
+from django.contrib import admin
+
+
+class ProductInline(admin.StackedInline):
+    """ отображение cart product в cart """
+    model = CartProduct
+    extra = 1
 
 
 @admin.register(Cart)
 class CartAdmin(admin.ModelAdmin):
-    """ Register Cart model in admin panel """
-    list_display = ('user', 'created_at')
-    list_display_links = ('user', 'created_at')
+    """ cart """
     save_on_top = True
 
+    inlines = [ProductInline]
+    list_display = ("user", "total_price", "created_at")
+    list_display_links = ("user", "total_price", "created_at")
 
-@admin.register(CartProduct)
-class CartProductAdmin(admin.ModelAdmin):
-    """ Register CartProduct model in admin panel """
-    list_display = ('cart', 'product', 'quantity', 'price', 'total_price')
-    list_display_links = ('cart', 'product')
-    save_on_top = True
+    def total_price(self, obj):
+        """Return the total price of all products in the cart"""
+        return '{:.1f}'.format(obj.total_price)
+
+    total_price.admin_order_field = 'total_price'
