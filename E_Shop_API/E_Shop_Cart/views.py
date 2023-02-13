f0a5901b40ea3c971f46d74259c40d8daface3c7
Qva777 <barshay.sasha@gmail.com>
2023-02-13 12:00:00 +0200

    Updated

--- E_Shop_API/E_Shop_Cart/views.py
+++ E_Shop_API/E_Shop_Cart/views.py
@@ -6,6 +6,7 @@ from django.urls import reverse
 from django.http import JsonResponse
 from .models import Cart, CartProduct
 import stripe
+
 # def add_to_cart(request, product_id):
 #     """Add a product to the cart"""
 #     cart, created = Cart.objects.get_or_create(user=request.user)
