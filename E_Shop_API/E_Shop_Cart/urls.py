d369443d2618ed81a3c43cf71e6b3e7cc1ffed8b
Qva777 <barshay.sasha@gmail.com>
2023-03-19 12:00:00 +0200

    Updated

--- E_Shop_API/E_Shop_Cart/urls.py
+++ E_Shop_API/E_Shop_Cart/urls.py
@@ -16,27 +16,3 @@ urlpatterns = [
     path('success_cart/', views.payment_success, name='payment_success'),
 ]
 
-# from django.urls import path, include
-#
-# from .views import CartView, CartProductListView
-# # from E_Shop_API.E_Shop_Products.views import CancelView, ErrorView
-# from E_Shop_API.E_Shop_Cart.views import ProductAddView, payment_cart
-#
-# urlpatterns = [
-#     # Cart post/get/delete
-#     path('cart/', CartView.as_view(), name='cart'),
-#
-#     # Create CartProduct
-#     path('product/add/', ProductAddView.as_view(), name='cart'),
-#
-#     # Auto Create Cart and add item to cart
-#     path('product/<int:product_id>/add/', ProductAddView.as_view(), name='add-to-cart'),
-#     # Payment
-#     path('product/my-cart/pay/', payment_cart, name='payment_cart'),
-#     # path('success/', success_cart, name='success'),
-#     # path('cancel/', CancelView.as_view(), name='cancel'),
-#     # path('error/', ErrorView.as_view(), name='error'),
-#     path('cart-products/', CartProductListView.as_view(), name='cart-product-list'),
-# ]
-#
-#
