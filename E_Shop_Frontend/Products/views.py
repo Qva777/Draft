8494d09d0a10df309dc191f92647ad852207c3d2
Qva777 <barshay.sasha@gmail.com>
2023-04-11 12:00:00 +0300

    Updated

--- E_Shop_Frontend/Products/views.py
+++ E_Shop_Frontend/Products/views.py
@@ -67,6 +67,8 @@ def search(request):
 #         products = paginator.get_page(page_number)
 #         return render(request, 'pages/home.html', {'products': products})
 
+
+
 class ProductHomeListView(View):
     """Main page/Product list"""
 
