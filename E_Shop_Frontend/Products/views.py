e2408f6ffe380005b86a2c2007dda35894a0f030
Qva777 <barshay.sasha@gmail.com>
2023-04-18 12:00:00 +0300

    Updated

--- E_Shop_Frontend/Products/views.py
+++ E_Shop_Frontend/Products/views.py
@@ -1,44 +1,90 @@
-import random
-from django.views.decorators.csrf import csrf_exempt
-from django.urls import reverse
-from django.shortcuts import get_object_or_404
-from django.http import JsonResponse
-from django.shortcuts import redirect
-import stripe
-from django.contrib import messages
-
-from django.conf import settings
-from django.views.generic import TemplateView
-
-from E_Shop_API.E_Shop_Cart.models import Cart, CartProduct
-from E_Shop_API.E_Shop_Products.models import Product
-from django.views import View
-from django.shortcuts import render
-from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
 from django.core.paginator import Paginator
+from django.http import JsonResponse
+from django.contrib import messages
+from django.conf import settings
+from django.urls import reverse
+from django.db.models import Q
 
+import random
+import stripe
+
+from django.views import View
+from django.views.generic import TemplateView
+from django.views.decorators.csrf import csrf_exempt
+
+from E_Shop_API.E_Shop_Products.models import Product
+from E_Shop_API.E_Shop_Cart.models import Cart, CartProduct
+
+# STRIPE KEY
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
 
-# work
-def search(request):
-    query = request.GET.get('q')
-    product_list = Product.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
-    if not request.user.is_staff:
-        product_list = product_list.filter(count__gt=0)
-    paginator = Paginator(product_list, 12)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-    return render(request, 'pages/search_results.html',
-                  {'query': query, 'page_obj': page_obj, 'product_list': product_list})
+class CancelProduct(TemplateView):
+    """Render error page"""
+    template_name = 'pages/not_found.html'
 
 
-class ProductHomeListView(View):
+class BaseProductView(View):
+    """Base view for product-related views"""
+
+    def get_cart(self, request):
+        if request.user.is_authenticated:
+            cart_queryset = Cart.objects.filter(user=request.user)
+            if cart_queryset.exists():
+                cart = cart_queryset.first()
+            else:
+                cart = Cart.objects.create(user=request.user)
+        else:
+            session_key = request.session.session_key
+            if not session_key:
+                request.session.cycle_key()
+            cart_queryset = Cart.objects.filter(session_key=session_key)
+            if cart_queryset.exists():
+                cart = cart_queryset.first()
+            else:
+                cart = Cart.objects.create(session_key=session_key)
+        return cart
+
+    def get_random_products(self):
+        if not self.request.user.is_staff:
+            random_products = Product.objects.filter(count__gt=0)
+            random_products = random.sample(list(random_products), 4)
+        else:
+            random_products = random.sample(list(Product.objects.all()), 4)
+        return random_products
+
+
+class SearchView(BaseProductView):
+    """Search field/Search_results"""
+
+    @staticmethod
+    def get(request):
+        query = request.GET.get('q')
+        product_list = Product.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
+
+        if not request.user.is_staff:
+            product_list = product_list.filter(count__gt=0)
+
+        paginator = Paginator(product_list, 12)
+        page_number = request.GET.get('page')
+        page_obj = paginator.get_page(page_number)
+
+        context = {
+            'query': query,
+            'page_obj': page_obj,
+            'product_list': product_list,
+        }
+
+        return render(request, 'pages/search_results.html', context)
+
+
+class ProductHomeListView(BaseProductView):
     """Main page/Product list"""
 
-    def get(self, request, *args, **kwargs):
+    @staticmethod
+    def get(request, *args, **kwargs):
         queryset = Product.objects.filter(active=True).order_by('-created_at')
-        # queryset = Product.objects.filter(active=True).order_by('?')  # generate Products by random
 
         if not request.user.is_staff:
             queryset = queryset.filter(count__gt=0)
@@ -53,70 +99,46 @@ class ProductHomeListView(View):
         return render(request, 'pages/home.html', {'products': products})
 
 
-class CancelProduct(TemplateView):
-    template_name = 'pages/not_found.html'
+class PaymentView(BaseProductView):
+    """Detail views of product"""
 
+    @csrf_exempt
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
 
-# №№№№№№№№№№№№№№№№№№№№№№№№№№№№№№ ВОООООРК start №№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№
+    def get_random_products(self):
+        opened_product_id = self.kwargs.get("product_id")
+        random_products = Product.objects.filter(active=True).exclude(id=opened_product_id).order_by('?')[:4]
+        return random_products
 
-@csrf_exempt
-def payment_pro(request, product_id):
-    product = get_object_or_404(Product, id=product_id)
+    def get(self, request, product_id):
+        product = get_object_or_404(Product, id=product_id)
+        cart = self.get_cart(request)
+        in_cart = CartProduct.objects.filter(cart=cart, product=product).exists()
+        cart_product_count = CartProduct.objects.filter(cart=cart).count()
 
-    random_products = random.sample(list(Product.objects.all()), 4)  # new  random
-    # cart = Cart.objects.get(user=request.user)
+        if cart_product_count >= 10:
+            messages.error(request, 'You have reached the maximum limit of 10 products in your cart.')
 
-    # if request.user.is_authenticated:
-    #     # cart = Cart.objects.get(user=request.user)
-    #     cart, _ = Cart.objects.get_or_create(user=request.user)
-    #     # cart = Cart.objects.get_or_create(user=request.user)
-    # else:
-    #     session_key = request.session.session_key
-    #     if not session_key:
-    #         request.session.cycle_key()
-    #     # cart = Cart.objects.create(session_key=session_key)
-    #     cart, _ = Cart.objects.get_or_create(session_key=session_key)
-    if request.user.is_authenticated:
-        cart_queryset = Cart.objects.filter(user=request.user)
-        if cart_queryset.exists():
-            cart = cart_queryset.first()
-        else:
-            cart = Cart.objects.create(user=request.user)
-    else:
-        session_key = request.session.session_key
-        if not session_key:
-            request.session.cycle_key()
-        cart_queryset = Cart.objects.filter(session_key=session_key)
-        if cart_queryset.exists():
-            cart = cart_queryset.first()
-        else:
-            cart = Cart.objects.create(session_key=session_key)
+        random_products = self.get_random_products()
 
-    in_cart = CartProduct.objects.filter(cart=cart, product=product).exists()
-    cart_product_count = CartProduct.objects.filter(cart=cart).count()
+        if product.count < 1:
+            error_url = reverse('404')
+            return redirect(error_url)
 
-    # if request.user.is_authenticated:
-    #     cart = Cart.objects.get(user=request.user)
-    #     in_cart = CartProduct.objects.filter(cart=cart, product=product).exists()
-    #     cart_product_count = CartProduct.objects.filter(cart=cart).count()
+        context = {
+            "product": product,
+            "stripe_public_key": settings.STRIPE_PUBLIC_KEY,
+            "random_products": random_products,
+            "in_cart": in_cart,
+            "cart_product_count": cart_product_count,
+        }
 
-    if cart_product_count >= 10:
-        messages.error(request, 'You have reached the maximum limit of 10 products in your cart.')
-        # return redirect('cart_detail')
+        return render(request, "pages/product_detail.html", context)
 
-    # work
-    if not request.user.is_staff:
-        random_products = Product.objects.filter(count__gt=0)
-        random_products = random.sample(list(random_products), 4)  # new random
-    else:
-        random_products = random.sample(list(Product.objects.all()), 4)  # new random
-
-    if product.count < 1:
-        error_url = reverse('404')  # , kwargs={'description': 'Product count is less than 1'})
-        return redirect(error_url)
-
-    if request.method == "POST":
-        # Use Stripe to charge the user's card
+    @staticmethod
+    def post(request, product_id):
+        product = get_object_or_404(Product, id=product_id)
         token = request.POST.get("stripeToken")
         amount = int(product.price * 100)
         try:
@@ -129,59 +151,111 @@ def payment_pro(request, product_id):
         except stripe.error.CardError as e:
             return JsonResponse({"error": e.user_message}, status=400)
 
-        # Update the product quantity
         product.count -= 1
         product.save()
 
-        # Return the updated product quantity in the response
-        return JsonResponse({"quantity": product.count})
-
-    context = {
-        "product": product,
-        "stripe_public_key": settings.STRIPE_PUBLIC_KEY,
-
-        "random_products": random_products,
-        "in_cart": in_cart,
-
-        "cart_product_count": cart_product_count,
-    }
-
-    return render(request, "pages/product_detail.html", context)
+        # return JsonResponse({"quantity": product.count})
+        return render(request, "pages/payment_success.html")
 
 # №№№№№№№№№№№№№№№№№№№№№№№№№№№№№№ ВОООООРК end №№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№
 
 
-# test card
-# Visa: 4242 4242 4242 4242
-# Mastercard: 5105 1051 0510 5100
-# American Express: 3782 822463 10005
-# Discover: 6011 1111 1111 1117
+# class PaymentView(View):
+#     """View for product payment"""
+#
+#     def get_stripe_public_key(self):
+#         """Helper method to get the Stripe public key"""
+#         return settings.STRIPE_PUBLIC_KEY
+#
+#     def get(self, request, product_id):
+#         # Handle GET requests to display the payment page (optional)
+#         product = get_object_or_404(Product, id=product_id)
+#         context = {
+#             "product": product,
+#             "stripe_public_key": self.get_stripe_public_key(),
+#         }
+#         return render(request, "pages/product_detail.html", context)
+#
+#     def post(self, request, product_id):
+#         product = get_object_or_404(Product, id=product_id)
+#         token = request.POST.get("stripeToken")
+#         amount = int(product.price * 100)
+#
+#         stripe.api_key = settings.STRIPE_SECRET_KEY
+#
+#         try:
+#             charge = stripe.Charge.create(
+#                 amount=amount,
+#                 currency="usd",
+#                 source=token,
+#                 description=product.name,
+#             )
+#         except stripe.error.CardError as e:
+#             return JsonResponse({"error": e.user_message}, status=400)
+#
+#         product.count -= 1
+#         product.save()
+#
+#         return JsonResponse({"quantity": product.count})
 
+# class PaymentView(View):
+#     """View for product payment"""
+#
+#     @csrf_exempt
+#     def dispatch(self, request, *args, **kwargs):
+#         return super().dispatch(request, *args, **kwargs)
+#
+#     def post(self, request, product_id):
+#         product = get_object_or_404(Product, id=product_id)
+#         token = request.POST.get("stripeToken")
+#         amount = int(product.price * 100)
+#
+#         try:
+#             charge = stripe.Charge.create(
+#                 amount=amount,
+#                 currency="usd",
+#                 source=token,
+#                 description=product.name,
+#             )
+#         except stripe.error.CardError as e:
+#             return JsonResponse({"error": e.user_message}, status=400)
+#
+#         product.count -= 1
+#         product.save()
+#
+#         return JsonResponse({"quantity": product.count})
 
-#  work
-# class ProductHomeListView(View):
-#     """ Main page/Product list """
+# class SearchView(View):
+#     """ Search field/Search_results """
 #
-#
-#     # ТУТ НУЖНО ПОЛУЧИТЬ КЛЮЧЬ СЕССИИ
-#
-#
-#     def get(self, request, *args, **kwargs):
-#         queryset = Product.objects.all().order_by('-created_at')
+#     @staticmethod
+#     def get(request):
+#         query = request.GET.get('q')
+#         product_list = Product.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
 #
 #         if not request.user.is_staff:
-#             queryset = queryset.filter(count__gt=0)
+#             product_list = product_list.filter(count__gt=0)
 #
-#         paginator = Paginator(queryset, 12)
+#         paginator = Paginator(product_list, 12)
 #         page_number = request.GET.get('page')
-#         products = paginator.get_page(page_number)
-#         return render(request, 'pages/home.html', {'products': products})
-#  work
+#         page_obj = paginator.get_page(page_number)
+#
+#         context = {
+#             'query': query,
+#             'page_obj': page_obj,
+#             'product_list': product_list,
+#         }
+#
+#         return render(request, 'pages/search_results.html', context)
+#
+#
 # class ProductHomeListView(View):
 #     """Main page/Product list"""
 #
-#     def get(self, request, *args, **kwargs):
-#         queryset = Product.objects.all().order_by('-created_at')
+#     @staticmethod
+#     def get(request, *args, **kwargs):
+#         queryset = Product.objects.filter(active=True).order_by('-created_at')
+#         # queryset = Product.objects.filter(active=True).order_by('?')  # generate Products by random
 #
 #         if not request.user.is_staff:
 #             queryset = queryset.filter(count__gt=0)
@@ -194,3 +268,93 @@ def payment_pro(request, product_id):
 #         page_number = request.GET.get('page')
 #         products = paginator.get_page(page_number)
 #         return render(request, 'pages/home.html', {'products': products})
+#
+#
+# class CancelProduct(TemplateView):
+#     """ Render error page """
+#     template_name = 'pages/not_found.html'
+#
+#
+#
+# class PaymentView(View):
+#     """ Detail views of product """
+#
+#     @csrf_exempt
+#     def dispatch(self, request, *args, **kwargs):
+#         return super().dispatch(request, *args, **kwargs)
+#
+#     @staticmethod
+#     def get(request, product_id):
+#         product = get_object_or_404(Product, id=product_id)
+#
+#         if request.user.is_authenticated:
+#             cart_queryset = Cart.objects.filter(user=request.user)
+#             if cart_queryset.exists():
+#                 cart = cart_queryset.first()
+#             else:
+#                 cart = Cart.objects.create(user=request.user)
+#         else:
+#             session_key = request.session.session_key
+#             if not session_key:
+#                 request.session.cycle_key()
+#             cart_queryset = Cart.objects.filter(session_key=session_key)
+#             if cart_queryset.exists():
+#                 cart = cart_queryset.first()
+#             else:
+#                 cart = Cart.objects.create(session_key=session_key)
+#
+#         in_cart = CartProduct.objects.filter(cart=cart, product=product).exists()
+#         cart_product_count = CartProduct.objects.filter(cart=cart).count()
+#
+#         if cart_product_count >= 10:
+#             messages.error(request, 'You have reached the maximum limit of 10 products in your cart.')
+#
+#         if not request.user.is_staff:
+#             random_products = Product.objects.filter(count__gt=0)
+#             random_products = random.sample(list(random_products), 4)
+#         else:
+#             random_products = random.sample(list(Product.objects.all()), 4)
+#
+#         if product.count < 1:
+#             error_url = reverse('404')
+#             return redirect(error_url)
+#
+#         context = {
+#             "product": product,
+#             "stripe_public_key": settings.STRIPE_PUBLIC_KEY,
+#             "random_products": random_products,
+#             "in_cart": in_cart,
+#             "cart_product_count": cart_product_count,
+#         }
+#
+#         return render(request, "pages/product_detail.html", context)
+#
+#     @staticmethod
+#     def post(request, product_id):
+#         product = get_object_or_404(Product, id=product_id)
+#         token = request.POST.get("stripeToken")
+#         amount = int(product.price * 100)
+#
+#         try:
+#             charge = stripe.Charge.create(
+#                 amount=amount,
+#                 currency="usd",
+#                 source=token,
+#                 description=product.name,
+#             )
+#         except stripe.error.CardError as e:
+#             return JsonResponse({"error": e.user_message}, status=400)
+#
+#         product.count -= 1
+#         product.save()
+#
+#         return JsonResponse({"quantity": product.count})
+
+# №№№№№№№№№№№№№№№№№№№№№№№№№№№№№№ ВОООООРК end №№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№№
+
+
+# test card
+# Visa: 4242 4242 4242 4242
+# Mastercard: 5105 1051 0510 5100
+# American Express: 3782 822463 10005
+# Discover: 6011 1111 1111 1117
