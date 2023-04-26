02ea66209017015b194725ce0d600c0bb81c3da6
Qva777 <barshay.sasha@gmail.com>
2023-04-26 12:00:00 +0300

    Updated

--- E_Shop_Frontend/Users/urls.py
+++ E_Shop_Frontend/Users/urls.py
@@ -1,21 +1,17 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from E_Shop_Frontend.Users.views import UserLoginView, RegistrationView, DeletePhotoView, \
-    EditProfileView
+from E_Shop_Frontend.Users import views
 
 urlpatterns = [
     # Signup
-    path('registration/', RegistrationView.as_view(), name='registration'),
+    path('registration/', views.RegistrationView.as_view(), name='registration'),
 
     # Login/Logout
-    path('login/', UserLoginView.as_view(), name='login'),
+    path('login/', views.UserLoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
 
-    # Your Profile
-    # path('profile/', edit_profile, name='user_profile'),
-
-    path('profile/', EditProfileView.as_view(), name='user_profile'),
-
-    path('profile/delete_photo/', DeletePhotoView.as_view(), name='delete_photo'),
+    # Edit User Profile
+    path('profile/', views.EditProfileView.as_view(), name='user_profile'),
+    path('profile/delete_photo/', views.DeletePhotoView.as_view(), name='delete_photo'),
 
 ]
