93e0ce55da9465aaf88f33ee82f720d2303e32fb
Qva777 <barshay.sasha@gmail.com>
2023-04-26 12:00:00 +0300

    Updated

--- E_Shop_Frontend/Products/urls.py
+++ E_Shop_Frontend/Products/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from E_Shop_Frontend.Products import views
 
 urlpatterns = [
-
-    # payment one product
-    path('product/<int:product_id>/pay/', views.payment_pro, name='payment_pro'),
-    # path('success_pro/', views.success, name='success_purchase'),
+    # Error page
     path('404/', views.CancelProduct.as_view(), name='404'),
-    path('search/', views.search, name='search'),
+
+    # Search View
+    path('search/', views.SearchView.as_view(), name='search'),
+
+    # Buy one product
+    path('product/<uuid:product_id>/pay/', views.PaymentView.as_view(), name='payment_pro'),
 ]
