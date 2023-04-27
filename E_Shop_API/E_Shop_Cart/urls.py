652b826479d91fbaf2e0b2163caa103e4167e2e1
Qva777 <barshay.sasha@gmail.com>
2023-04-27 12:00:00 +0300

    Updated

--- E_Shop_API/E_Shop_Cart/urls.py
+++ E_Shop_API/E_Shop_Cart/urls.py
@@ -2,17 +2,11 @@ from django.urls import path
 from E_Shop_API.E_Shop_Cart import views
 
 urlpatterns = [
-    #
-    path('cart_detail/', views.cart_detail, name='cart_detail'),
-    path('add/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
-    path('remove/<int:product_id>/', views.remove_cart, name='remove_from_cart'),
+    # CRUD cart  /api/
+    path('cart/<uuid:cart_product_id>/', views.CartProductAPIView.as_view(), name='cart-product-api'),  # add doesn't work
 
-    path('empty_cart/', views.empty_cart, name='empty_cart'),
+    path('cart/', views.CartProductListAPIView.as_view(), name='cart-detail-api'),  # bad
 
-    path('update-cart/<int:product_id>/', views.update_cart, name='update_cart'),
-    # zim
-    # path('cart/pay/', views.pay_cart, name='pay_cart'),
-    path('cart/payment/', views.payment_cart, name='payment_cart'),
-    path('success_cart/', views.payment_success, name='payment_success'),
+    path('payment/', views.PaymentCartAPIView.as_view(), name='payment'),
 ]
 
