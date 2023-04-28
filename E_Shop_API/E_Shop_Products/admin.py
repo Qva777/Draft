fa9ad594138523eb30ef15b043c762a058930bf5
Qva777 <barshay.sasha@gmail.com>
2023-04-28 12:00:00 +0300

    Updated

--- E_Shop_API/E_Shop_Products/admin.py
+++ E_Shop_API/E_Shop_Products/admin.py
@@ -1,26 +1,34 @@
 from django.contrib import admin
-from django.utils.html import mark_safe
-from E_Shop_API.E_Shop_Products.models import Product
+from django.utils.html import format_html
+from E_Shop_API.E_Shop_Products.models import Product, ProductImage
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 0
 
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    """ Custom product field in admin panel """
     list_display = ('get_photo', 'name', 'count', 'price', 'updated_at', 'active')
     list_display_links = ('name',)
     search_fields = ('name', 'description')
     list_editable = ('active',)
     list_filter = ('description', 'price')
-    readonly_fields = ('get_photo', 'created_at', 'updated_at')
+    readonly_fields = ('created_at', 'updated_at')
     save_on_top = True
-
     list_per_page = 20
+    inlines = [ProductImageInline]
 
     def get_photo(self, obj):
-        """ Method which return img in admin panel """
-        if obj.photo:
-            return mark_safe(f'<img src="{obj.photo.url}" width="100">')
-        else:
-            return '-'
+        """ Method to return image in admin panel """
+        if obj.photos.exists():
+            # Assuming you want to display the first photo
+            photo = obj.photos.first()
+            return format_html('<img src="{}" width="100">', photo.image.url)
+        return '-'
 
     get_photo.short_description = 'Photo'
+
+
+admin.site.register(ProductImage)
