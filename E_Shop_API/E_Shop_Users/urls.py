9cf16470d92df9d6f05d858ab57192c6a3ef2fca
Qva777 <barshay.sasha@gmail.com>
2023-04-30 12:00:00 +0300

    Updated

--- E_Shop_API/E_Shop_Users/urls.py
+++ E_Shop_API/E_Shop_Users/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from E_Shop_API.E_Shop_Users.views import MyUserView, UserDetailView, SiteView, SelectSocialApplicationView
 
 urlpatterns = [
@@ -6,18 +6,7 @@ urlpatterns = [
     path('auth/users/me/', MyUserView.as_view(), name='my_user_view'),
     path('auth/users/<uuid:pk>/', UserDetailView.as_view(), name='user_detail_view'),
 
-    # GOOGLE
+    # GOOGLE OAUTH PROVIDER
     path('sites/<int:pk>/', SiteView.as_view(), name='site_detail'),
     path('provider/<int:pk>/', SelectSocialApplicationView.as_view(), name='select_social_application'),
-
-
-    # preparing to delete
-    # path('auth/', include('djoser.urls')),
-    # path('auth/token/', CustomTokenCreateView.as_view(), name='token_create'),
-    # path('auth/users/', CustomUserViewSet.as_view({'post': 'create'}), name='user_create'),
-    # path('auth/register/', MyUserCreateView.as_view(), name='register'),
-    # path('auth/login/', MyTokenCreateView.as_view(), name='login'),
-
-    # path('register/', register, name='register'),
-    # path('login/', user_login, name='login'),
 ]
