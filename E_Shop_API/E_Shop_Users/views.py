98531e7b503cae993a95075b99557b68dcd2f41a
Qva777 <barshay.sasha@gmail.com>
2023-05-02 12:00:00 +0300

    Updated

--- E_Shop_API/E_Shop_Users/views.py
+++ E_Shop_API/E_Shop_Users/views.py
@@ -2,14 +2,13 @@ from allauth.socialaccount.models import SocialApp
 from django.contrib.sites.models import Site
 from django.http import JsonResponse
 
-from rest_framework import permissions
+from rest_framework import permissions, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.generics import RetrieveUpdateAPIView
 
+from E_Shop_API.E_Shop_Users import serializers
 from E_Shop_API.E_Shop_Users.models import Clients
-from E_Shop_API.E_Shop_Users.serializers import MyUserSerializer, UserDetailSerializer, SocialAppSerializer, \
-    SiteSerializer
 
 
 class UserDetailView(APIView):
@@ -20,36 +19,46 @@ class UserDetailView(APIView):
     def get(request, pk):
         """ GET Method user/<int:pk>/ """
         user = Clients.objects.get(pk=pk)
-        serializer = UserDetailSerializer(user)
+        serializer = serializers.UserDetailSerializer(user)
         return Response(serializer.data)
 
     @staticmethod
     def put(request, pk):
         """ PUT Method user/<int:pk>/ """
         user = Clients.objects.get(pk=pk)
-        serializer = MyUserSerializer(user, data=request.data)
+        serializer = serializers.MyUserSerializer(user, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data)
-
-    #     return JsonResponse(serializer.errors, status=400)
 
     @staticmethod
     def patch(request, pk):
         """ PATCH Method user/<int:pk>/ """
         user = Clients.objects.get(pk=pk)
-        serializer = MyUserSerializer(user, data=request.data, partial=True)
+        serializer = serializers.MyUserSerializer(user, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data)
-        # return JsonResponse(serializer.errors, status=400)
 
     @staticmethod
     def delete(request, pk):
-        """ DELETE Method user/<int:pk>/ """
+        """ DELETE/HIDE Method user/<int:pk>/ """
         user = Clients.objects.get(pk=pk)
-        user.delete()
-        return Response(status=204)
+        if user.is_active:
+            user.is_active = False
+            user.save()
+            return Response({'message': 'User deactivated'}, status=status.HTTP_200_OK)
+        return Response({'message': 'User is already deactivate'}, status=status.HTTP_400_BAD_REQUEST)
+
+    @staticmethod
+    def post(request, pk):
+        """ Activate a disabled user """
+        user = Clients.objects.get(pk=pk)
+        if not user.is_active:
+            user.is_active = True
+            user.save()
+            return Response({'message': 'User activated'}, status=status.HTTP_200_OK)
+        return Response({'message': 'User is already active'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 class MyUserView(APIView):
@@ -60,14 +69,14 @@ class MyUserView(APIView):
         """ GET Method user 'auth/users/me/' """
         get_user = self.request.user
         user = Clients.objects.get(pk=get_user.pk)
-        serializer = UserDetailSerializer(user)
+        serializer = serializers.UserDetailSerializer(user)
         return JsonResponse(serializer.data)
 
     def put(self, request):
         """ PUT Method user 'auth/users/me/' """
         get_user = self.request.user
         user = Clients.objects.get(pk=get_user.pk)
-        serializer = MyUserSerializer(user, data=request.data)
+        serializer = serializers.MyUserSerializer(user, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return JsonResponse(serializer.data)
@@ -77,7 +86,7 @@ class MyUserView(APIView):
         """ PATCH Method user 'auth/users/me/' """
         get_user = self.request.user
         user = Clients.objects.get(pk=get_user.pk)
-        serializer = MyUserSerializer(get_user, data=request.data, partial=True)
+        serializer = serializers.MyUserSerializer(get_user, data=request.data, partial=True)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
@@ -86,33 +95,33 @@ class MyUserView(APIView):
     def delete(cls, request):
         """ HIDE Method user 'auth/users/me/' """
         user = request.user
-        user.is_active = False
-        user.save()
-        #     user.delete()
-        #  return active by email
-        return Response(status=204)
+        if user.is_active:
+            user.is_active = False
+            user.save()
+            return Response({'message': 'User deactivated'}, status=status.HTTP_200_OK)
+        return Response({'message': 'User is already deactivated'}, status=status.HTTP_400_BAD_REQUEST)
 
 
 # GOOGLE OAUTH PROVIDER
 class SiteView(RetrieveUpdateAPIView):
-    """ A view to retrieve and update site details """
+    """ Domain site """
     permission_classes = [permissions.IsAdminUser, ]
 
     queryset = Site.objects.all()
-    serializer_class = SiteSerializer
+    serializer_class = serializers.SiteSerializer
     lookup_field = 'pk'
 
     def update(self, request, *args, **kwargs):
-        """ Update site details """
+        """ Update site """
         site = Site.objects.get(pk=kwargs['pk'])
         site.domain = request.data.get('domain')
         site.name = request.data.get('name')
         site.save()
-        return Response(SiteSerializer(site).data)
+        return Response(serializers.SiteSerializer(site).data)
 
 
 class SelectSocialApplicationView(APIView):
-    """ A view to retrieve and create a social application """
+    """ GOOGLE OAUTH PROVIDER """
     permission_classes = [permissions.IsAdminUser, ]
 
     @staticmethod
@@ -120,7 +129,7 @@ class SelectSocialApplicationView(APIView):
         """ Retrieve a social app by ID """
         try:
             social_app = SocialApp.objects.get(pk=pk)
-            serializer = SocialAppSerializer(social_app)
+            serializer = serializers.SocialAppSerializer(social_app)
             return Response(serializer.data)
         except SocialApp.DoesNotExist:
             return Response({"error": "Social application with id {} does not exist".format(pk)})
@@ -128,7 +137,7 @@ class SelectSocialApplicationView(APIView):
     @staticmethod
     def post(request, pk):
         """ Create a social app with provided data """
-        serializer = SocialAppSerializer(data=request.data)
+        serializer = serializers.SocialAppSerializer(data=request.data)
         if serializer.is_valid():
             social_app = serializer.save()
             return Response(serializer.data)
