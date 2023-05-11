51080f6d3f6de5ea4301876f8dee37905da6b059
Qva777 <barshay.sasha@gmail.com>
2023-05-11 12:00:00 +0300

    Updated

--- E_Shop_config/urls.py
+++ E_Shop_config/urls.py
@@ -41,7 +41,7 @@ urlpatterns = [
     # API APPLICATION
     path('api/', include('E_Shop_API.E_Shop_Users.urls')),
     path('api/', include('E_Shop_API.E_Shop_Products.urls')),
-    path('api/', include('E_Shop_API.E_Shop_Cart.urls')),  ################
+    path('api/', include('E_Shop_API.E_Shop_Cart.urls')),
 
     # DJOSER
     path('api/auth/', include('djoser.urls')),
