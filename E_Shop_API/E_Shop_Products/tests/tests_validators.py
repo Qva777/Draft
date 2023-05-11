53fd880594aa71c8484c998aa521b56b78ab07f0
Qva777 <barshay.sasha@gmail.com>
2023-05-11 12:00:00 +0300

    Updated

--- E_Shop_API/E_Shop_Products/tests/tests_validators.py
+++ E_Shop_API/E_Shop_Products/tests/tests_validators.py
@@ -1,21 +1,28 @@
 # python manage.py test E_Shop_API.E_Shop_Products.tests.tests_validators
 from django.test import TestCase
 from django.core.exceptions import ValidationError
+
 from E_Shop_API.E_Shop_Products.validators import validate_negative
+from E_Shop_API.E_Shop_Products.tests.ErrorMessage.product_enums import ProductErrorMessages
 
 
 class ValidatorsTestCase(TestCase):
-    def test_validate_negative_positive_value(self):
-        """ Should return True for positive values """
-        result = validate_negative(10)
-        self.assertTrue(result)
+    def test_positive_value_does_not_raise_error(self):
+        """ Positive values should not raise a ValidationError when using validate_negative """
+        try:
+            validate_negative(10)
+        except ValidationError as e:
+            self.fail(f"Unexpected ValidationError raised: {e}")
 
-    def test_validate_negative_zero_value(self):
-        """ Should return False for zero value """
+    def test_zero_value_returns_false(self):
+        """ Zero values should return False when using validate_negative """
         result = validate_negative(0)
         self.assertFalse(result)
 
-    def test_validate_negative_negative_value(self):
-        """ Should raise a ValidationError """
-        with self.assertRaises(ValidationError):
+    def test_negative_value_raises_error(self):
+        """ Negative values should raise a ValidationError when using validate_negative """
+        with self.assertRaises(ValidationError) as cm:
             validate_negative(-10)
+
+        expected_error_message = [ProductErrorMessages.NEGATIVE_VALUE]
+        self.assertEqual(cm.exception.messages, expected_error_message)
