eefcb00160a0fdf1a13ac5028b54e80b98f341aa
Qva777 <barshay.sasha@gmail.com>
2023-05-11 12:00:00 +0300

    Updated

--- E_Shop_API/E_Shop_Products/validators.py
+++ E_Shop_API/E_Shop_Products/validators.py
@@ -1,12 +1,14 @@
 from django.core.exceptions import ValidationError
 
+from E_Shop_API.E_Shop_Products.tests.ErrorMessage.product_enums import ProductErrorMessages
+
 
 def validate_negative(value):
     """
     Validator function to check the product price/count and set the 'active' field accordingly.
     """
     if value < 0:
-        raise ValidationError('Quantity cannot be negative.')
+        raise ValidationError(ProductErrorMessages.NEGATIVE_VALUE)
     elif value == 0:
         active = False
     else:
