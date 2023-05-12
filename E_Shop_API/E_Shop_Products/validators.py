2aa9f1ee5f9a0dcd9cfc19d954e0d42b7ca07136
Qva777 <barshay.sasha@gmail.com>
2023-05-12 12:00:00 +0300

    Updated

--- E_Shop_API/E_Shop_Products/validators.py
+++ E_Shop_API/E_Shop_Products/validators.py
@@ -4,15 +4,11 @@ from E_Shop_API.E_Shop_Products.tests.ErrorMessage.product_enums import ProductE
 
 
 def validate_negative(value):
-    """
-    Validator function to check the product price/count and set the 'active' field accordingly.
-    """
+    """ Validator function to check the product price/count and set the 'active' field accordingly """
     if value < 0:
-        raise ValidationError(ProductErrorMessages.NEGATIVE_VALUE)
+        raise ValidationError(ProductErrorMessages.NEGATIVE_VALUE)  # ?
     elif value == 0:
         active = False
     else:
         active = True
     return active
-
-
