40add8addd8aa92d8191b4cda03878456113b228
Qva777 <barshay.sasha@gmail.com>
2023-05-14 12:00:00 +0300

    Updated

--- E_Shop_API/E_Shop_Users/urls.py
+++ E_Shop_API/E_Shop_Users/urls.py
@@ -1,12 +1,21 @@
 from django.urls import path
-from E_Shop_API.E_Shop_Users.views import MyUserView, UserDetailView, SiteView, SelectSocialApplicationView
+from E_Shop_API.E_Shop_Users import views
 
 urlpatterns = [
     # CRUD
-    path('auth/users/me/', MyUserView.as_view(), name='my_user_view'),
-    path('auth/users/<uuid:pk>/', UserDetailView.as_view(), name='user_detail_view'),
+    path('auth/users/me/', views.MyUserView.as_view(), name='my_user_view'),
+    path('auth/users/<uuid:pk>/', views.UserDetailView.as_view(), name='user_detail_view'),
+
+
+    # Activate Account
+    path('send_activation/', views.SendActivationView.as_view(), name='send_activation'),
+    path('activate_user/', views.ActivateUserView.as_view(), name='activate_user'),
+
+    # Forgot password
+    path('forgot_password/', views.ForgotPasswordAPI.as_view()),
+
 
     # GOOGLE OAUTH PROVIDER
-    path('sites/<int:pk>/', SiteView.as_view(), name='site_detail'),
-    path('provider/<int:pk>/', SelectSocialApplicationView.as_view(), name='select_social_application'),
+    path('sites/<int:pk>/', views.SiteView.as_view(), name='site_detail'),
+    path('provider/<int:pk>/', views.SelectSocialApplicationView.as_view(), name='select_social_application'),
 ]
