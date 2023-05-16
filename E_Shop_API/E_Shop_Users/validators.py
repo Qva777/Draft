2a84fa21f5b2a57a6d08a086bf9010fb46198cee
Qva777 <barshay.sasha@gmail.com>
2023-05-16 12:00:00 +0300

    Add test_validators

--- E_Shop_API/E_Shop_Users/validators.py
+++ E_Shop_API/E_Shop_Users/validators.py
@@ -1,24 +1,34 @@
 import re
-import datetime
 from datetime import datetime, date
 from django.core.exceptions import ValidationError
+
+from E_Shop_API.E_Shop_Users.tests.helpers.error_messages import ErrorMessages
 
 
 def validate_password(value):
     """ Password validator """
+    error_messages = []
+
     if not re.search("[A-Z]", value):
-        raise ValidationError("Password must contain at least one upper case letter.")
+        error_messages.append(ErrorMessages.UPPER_CASE_LETTER)
     if not re.search("[0-9]", value):
-        raise ValidationError("Password must contain at least one digit.")
+        error_messages.append(ErrorMessages.AT_LEAST_ONE_DIGIT)
     if len(value) < 8:
-        raise ValidationError("Password must be at least 8 characters long.")
+        error_messages.append(ErrorMessages.MIN_8_CHARACTERS)
+    if error_messages:
+        raise ValidationError(error_messages)
 
 
 def birthday_validator(value):
     """Birthday Validator"""
-    # date_obj = datetime.strptime(value, "%Y-%m-%d").date()
+    error_messages = []
+
     today = date.today()
     if value > today:
-        raise ValidationError('Cannot be in the future.')
-    elif value < datetime.strptime('1930-01-01', "%Y-%m-%d").date():
-        raise ValidationError('This year is not valid. ')
+        error_messages.append(ErrorMessages.CANNOT_BE_FUTURE)
+
+    if value < datetime.strptime('1970-01-01', "%Y-%m-%d").date():
+        error_messages.append(ErrorMessages.YEAR_IS_NOT_VALID)
+
+    if error_messages:
+        raise ValidationError(error_messages)
