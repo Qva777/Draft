3c8643998167859d294e2be8bbe9695c85484378
Qva777 <barshay.sasha@gmail.com>
2023-08-25 12:00:00 +0300

    Wrote tests on Frontend Users urls

--- E_Shop_Frontend/Users/tests/test_urls.py
+++ E_Shop_Frontend/Users/tests/test_urls.py
@@ -1,39 +1,35 @@
 from django.test import TestCase
 from django.urls import reverse
-from django.contrib.auth import get_user_model
+
+from E_Shop_API.E_Shop_Users.tests.helpers.test_helpers import create_basic_user
 
 
 class UsersURLsAndViewsTest(TestCase):
     def setUp(self):
-        # Create a test user
-        self.user = get_user_model().objects.create_user(
-            username='testuser',
-            email='test@example.com',
-            password='testpassword'
-        )
+        self.user = create_basic_user()
 
     def test_registration_view(self):
-        # Test the registration view
+        """ Test the registration view """
         response = self.client.get(reverse('registration'))
         self.assertEqual(response.status_code, 200)
 
     def test_forgot_password_view(self):
-        # Test the forgot_password view
+        """ Test the forgot_password view """
         response = self.client.get(reverse('forgot_password'))
         self.assertEqual(response.status_code, 200)
 
     def test_password_reset_view(self):
-        # Test the password_reset view
+        """ Test the password_reset view """
         response = self.client.get(reverse('password_reset'))
         self.assertEqual(response.status_code, 302)
 
     def test_resend_confirmation_view(self):
-        # Test the resend_confirmation view
+        """ Test the resend_confirmation view """
         response = self.client.get(reverse('resend_confirmation'))
         self.assertEqual(response.status_code, 405)
 
     def test_confirm_account_view(self):
-        # Test the confirm_account view with valid UID and token
+        """ Test the confirm_account view with valid UID and token """
         uid = self.user.pk
         token = 'valid-token'  # Replace with a valid token
         url = reverse('confirm_account', args=[uid, token])
@@ -41,26 +37,26 @@ class UsersURLsAndViewsTest(TestCase):
         self.assertEqual(response.status_code, 302)  # Redirects to the home page
 
     def test_login_view(self):
-        # Test the login view
+        """ Test the login view """
         response = self.client.get(reverse('login'))
         self.assertEqual(response.status_code, 200)
 
     def test_logout_view(self):
-        # Test the logout view
+        """ Test the logout view """
         response = self.client.get(reverse('logout'))
         self.assertEqual(response.status_code, 302)  # Redirects to the home page
 
     def test_user_profile_view(self):
-        # Log in the user before testing the user_profile view
-        self.client.login(username='testuser', password='testpassword')
+        """ Log in the user before testing the user_profile view """
+        self.client.login(username='user@gmail.com', password='UserPass123')
 
         # Test the user_profile view
         response = self.client.get(reverse('user_profile'))
         self.assertEqual(response.status_code, 200)
 
     def test_delete_photo_view(self):
-        # Log in the user before testing the delete_photo view
-        self.client.login(username='testuser', password='testpassword')
+        """ Log in the user before testing the delete_photo view """
+        self.client.login(username='user@gmail.com', password='UserPass123')
 
         # Test the delete_photo view
         response = self.client.post(reverse('delete_photo'))
