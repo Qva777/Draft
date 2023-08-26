ce881dd72d8f75b03b002b617cbc2a3ff2d109e5
Qva777 <barshay.sasha@gmail.com>
2023-08-26 12:00:00 +0300

    Updated

--- E_Shop_API/E_Shop_Products/tests/helpers/test_helpers.py
+++ E_Shop_API/E_Shop_Products/tests/helpers/test_helpers.py
@@ -2,6 +2,8 @@ from E_Shop_API.E_Shop_Cart.models import Product
 
 
 def create_product():
+    """ Create and return a Product for testing """
+
     return Product.objects.create(
         name='Test Product',
         description='Test Product Description',
