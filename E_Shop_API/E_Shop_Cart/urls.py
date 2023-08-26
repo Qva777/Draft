b713bd13767106b0345c3ccdbd4dbf9a10590e75
Qva777 <barshay.sasha@gmail.com>
2023-08-26 12:00:00 +0300

    Urls were cleand

--- E_Shop_API/E_Shop_Cart/urls.py
+++ E_Shop_API/E_Shop_Cart/urls.py
@@ -3,9 +3,11 @@ from E_Shop_API.E_Shop_Cart import views
 
 urlpatterns = [
     # CRUD cart  /api/
-    path('cart/<uuid:cart_product_id>/', views.CartProductAPIView.as_view(), name='cart-product-api'),  # add doesn't work
+    path('cart/<uuid:cart_product_id>/', views.CartProductAPIView.as_view(), name='cart_product_api'),
 
-    path('cart/', views.CartProductListAPIView.as_view(), name='cart-detail-api'),  # bad
+    # Cart list view
+    path('cart/', views.CartProductListAPIView.as_view(), name='cart_list_api'),
 
-    path('payment/', views.PaymentCartAPIView.as_view(), name='payment'),
+    # Cart payment processing
+    path('payment/', views.PaymentCartAPIView.as_view(), name='payment_api'),
 ]
